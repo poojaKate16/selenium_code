6907be6708b6bfd18a6554281809a0ed538f59ae
poojaKate16 <poojagavali1666@gmail.com>
2025-11-10 16:48:08 +0530

    Add files via upload

---
+++ Sescript.java
@@ -0,0 +1,12 @@
+package sproject;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sescript {
+
+public static void main(String[] args) {
+		
+ChromeDriver driver=new ChromeDriver();
+driver.get("https://www.google.com/");
+	}
+
+}
